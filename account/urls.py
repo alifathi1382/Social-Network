109e5d7187f75438049b18c6c8b7d6f3f3cc616e
ali-fathi <alifathi13821382@gmail.com>
2024-04-08 22:36:40 +0330

    Initial commit

---
+++ account/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+app_name = 'account'
+urlpatterns = [
+    path('register/', views.UserRegisterView.as_view(), name='register'),
+    path('login/', views.UserLoginView.as_view(), name='login'),
+    path('logout/', views.UserLogoutView.as_view(), name='logout'),
+    path('profile/<int:user_id>', views.UserProfilView.as_view(), name='profile'),
+    path('follow/<int:user_id>/', views.UserFollowView.as_view(), name='user_follow'),
+    path('unfollow/<int:user_id>/', views.UserUnfollowView.as_view(), name='unfollow'),
+    path('edit_user/',views.EditUserView.as_view(),name='edit_user')
+
+]
---
+++ home/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    body = models.TextField()
+    slug = models.SlugField()
+    title =models.CharField(max_length=30,null=True,blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.slug}....{self.update}"
+
+    class Meta:
+        ordering = ['body']
+
+    def likes_count(self):
+        return self.pvotes.count()
+
+    def user_can_like(self, user):
+        user_like = user.uvotes.filter(post=self)
+        if user_like.exists():
+            return True
+        return False
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ucomments')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='pcomments')
+    reply = models.ForeignKey('self', on_delete=models.CASCADE, related_name='rcomments', blank=True, null=True)
+    is_reply = models.BooleanField(default=False)
+    body = models.TextField(max_length=400)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user} - {self.body[:30]}'
+
+
+class Vote(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='uvotes')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='pvotes')
+
+    def __str__(self):
+        return f'{self.user} liked {self.post.slug}'
